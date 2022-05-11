da55ee5c045b115c0be0032732c43960bb395534
sayyyyy <190781001@ccmailg.meijo-u.ac.jp>
2022-05-11 15:44:48 +0900

    会員登録処理追加、表示するものがないため一時的にgetで追加できるようにしている

--- api-server/src/run.py
+++ api-server/src/run.py
@@ -1,6 +1,7 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request, url_for
 from models import db, User
 from flask_migrate import Migrate
+import flask_login
 import hashlib
 
 def create_app():
@@ -10,15 +11,94 @@ def create_app():
     # SQLAlchemy設定
     db.init_app(app)
     Migrate(app, db)
+    
 
     return app
 
 
 app = create_app()
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
 
 @app.route('/')
 def test():
     return 'test'
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == "POST":
+        # email = request.form.get('email')
+        # name = request.form.get('name')
+        # password = request.form.get('password')
+
+        email = 'test@a.a'
+        name = 'test'
+        password = 'password'
+
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            return redirect(url_for('signup'))
+
+        new_user = User(name=name, password=hashlib.sha256(password.encode('utf-8')).hexdigest(), email=email)
+        
+        db.session.add(new_user)
+        db.session.commit()
+
+        add_user = User.query.filter_by(name=name, password=hashlib.sha256(password.encode('utf-8')).hexdigest()).first()
+
+        if not add_user:
+            print('追加に失敗しました')
+            return '追加に失敗しました'
+            # return redirect('/signup')
+
+        flask_login.login_user(new_user)
+
+
+        return redirect(url_for('main'))
+    else:
+
+        # フロント側の処理が完成したらGETメソッドの処理は削除する
+
+        email = 'test@a.a'
+        name = 'test'
+        password = 'password'
+
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            print('そのメールアドレスは使われています')
+            return 'そのメールアドレスは使われています'
+            # return redirect('/signup')
+
+        new_user = User(name=name, password=hashlib.sha256(password.encode('utf-8')).hexdigest(), email=email)
+        
+        db.session.add(new_user)
+        db.session.commit()
+
+        add_user = User.query.filter_by(name=name, password=hashlib.sha256(password.encode('utf-8')).hexdigest()).first()
+
+        if not add_user:
+            print('追加に失敗しました')
+            return '追加に失敗しました'
+            # return redirect('/signup')
+
+        flask_login.login_user(new_user)
+
+        return redirect('main')
+
+@app.route('/main')
+@flask_login.login_required
+def main_isLogin():
+    return 'ログインしています'
+
+@login_manager.unauthorized_handler
+def main_isntLogin():
+    return 'ログインしていません'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 if __name__ == '__main__':
     app.run()
