a1be3fa0eff16730151d7fc6835fd4dfb2114afc
guchio33 <aokihikari3@gmail.com>
2022-05-16 17:08:06 +0900

    ホーム画面のボタン設置

--- front/src/view/pages/Home.js
+++ front/src/view/pages/Home.js
@@ -1,8 +1,22 @@
 import Footer from "../components/Footer";
+import classes from "./../../style/page/Home.module.css"
 const Home=()=>{
   return(
     <>
       <h1>ホーム画面</h1>
+      <button className={classes.button}>
+       <img src={`${process.env.PUBLIC_URL}/img/roombattle1.png`} alt="ルームボタン" className={classes.roomimg} />
+      </button>
+
+      <button className={classes.ratebutton}>
+       <img src={`${process.env.PUBLIC_URL}/img/rate.png`} alt="ルームボタン" className={classes.rateimg} />
+      </button>
+
+      
+
+      <div>
+
+      </div>
       <Footer />
     </>
   )
