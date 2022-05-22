fa08fb3bcaf13dbfc930413f333aa17dfd89c92a
hibiki esaki <61895243+hibiki0612@users.noreply.github.com>
2022-05-22 14:29:35 +0900

    Update camera.py

--- api-server/src/camera.py
+++ api-server/src/camera.py
@@ -1,43 +1,35 @@
-import cv2
-from unittest import result
-from numpy import imag
-import mediapipe as mp
-import os
+import bdb
+from contextlib import nullcontext
+import math
+from re import A
+from matplotlib.cbook import print_cycles
+import numpy as np
 
 
-class VideoCamera(object):
-    def __init__(self):
-        self.video = cv2.VideoCapture(0)
-        self.mp_pose = mp.solutions.pose
-        self.pose = self.mp_pose.Pose()
-        self.mp_drawing = mp.solutions.drawing_utils
-        self.mp_pose = mp.solutions.pose
+def angle(left_shoulder,left_hip,count):
+    price = False
 
-        # Opencvのカメラをセットします。(0)はノートパソコンならば組み込まれているカメラ
+    waist = left_hip
+    after_neck = left_shoulder
+    
+    vec1 = [1-waist.x,0-waist.y]
+    vec2 = [after_neck.x-waist.x,after_neck.y-waist.y]
+    absvec1=np.linalg.norm(vec1)
+    absvec2=np.linalg.norm(vec2)
+    inner=np.inner(vec1,vec2)
+    cos_theta=inner/(absvec1*absvec2)
+    theta=math.degrees(math.acos(cos_theta))
+    game_count =count_angle(price,count,theta)
+    print(count)
+    print('angle='+str(round(theta,2))+'deg')
 
-    def __del__(self):
-        self.video.release()
+    return game_count
 
-    def get_frame(self):
-        ret,frame = self.video.read()
-        #image = run_web.run(image)
-        frame = cv2.flip(frame,1)
-        image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
-        results = self.pose.process(image)
-        image.flags.writeable = True
-        image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
-        image_width = image.shape[0]
-        image_height = image.shape[1]
-        self.mp_drawing.draw_landmarks(
-        image, results.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
+def count_angle(price,count,theta):
 
-        ret, jpeg = cv2.imencode('.jpg', image)
-        
-        return jpeg.tobytes()
-
-        # read()は、二つの値を返すので、success, imageの2つ変数で受けています。
-        # OpencVはデフォルトでは raw imagesなので JPEGに変換
-        # ファイルに保存する場合はimwriteを使用、メモリ上に格納したい時はimencodeを使用
-        # cv2.imencode() は numpy.ndarray() を返すので .tobytes() で bytes 型に変換
-
+    if theta >= 60 and price == False:
+        #game_count +=1
+        price = True
+    elif theta < 10 and price == True:
+        price = False
+    return count
